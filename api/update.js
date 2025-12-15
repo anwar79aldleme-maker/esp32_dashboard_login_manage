410e00d3632826b0b12bb459cc9b6ff84f3a55bb
anwar79aldleme-maker <anwar79aldleme@gmail.com>
2025-12-15 06:06:24 +0000

    Update update.js

--- api/update.js
+++ api/update.js
@@ -1,1 +1,13 @@
+import { neon } from "@neondatabase/serverless";
+const sql = neon(process.env.DATABASE_URL);
+
+export default async function handler(req,res){
+  const { id, spo2, heartrate } = req.body;
+  await sql`
+    UPDATE sensor_data
+    SET spo2=${spo2}, heartrate=${heartrate}
+    WHERE id=${id}
+  `;
+  res.json({ success:true });
+}
 
