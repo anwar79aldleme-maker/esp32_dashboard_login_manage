856bbc1283b7b7092ac0f453db13b2e223bf5c7f
anwar79aldleme-maker <anwar79aldleme@gmail.com>
2025-12-15 06:06:54 +0000

    Update delete.js

--- api/delete.js
+++ api/delete.js
@@ -1,1 +1,9 @@
+import { neon } from "@neondatabase/serverless";
+const sql = neon(process.env.DATABASE_URL);
+
+export default async function handler(req,res){
+  const { id } = req.body;
+  await sql`DELETE FROM sensor_data WHERE id=${id}`;
+  res.json({ success:true });
+}
 
