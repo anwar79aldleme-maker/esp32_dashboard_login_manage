c48b5f427a4146c508d61534b0e4ecdd1666bbf1
anwar79aldleme-maker <anwar79aldleme@gmail.com>
2025-12-15 06:05:33 +0000

    Update add.js

--- api/add.js
+++ api/add.js
@@ -1,1 +1,17 @@
+import { neon } from "@neondatabase/serverless";
+const sql = neon(process.env.DATABASE_URL);
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST')
+    return res.status(405).json({ message:'Method not allowed' });
+
+  const { device_id,pname,pmobile,spo2,heartrate } = req.body;
+
+  await sql`
+    INSERT INTO sensor_data(device_id,pname,pmobile,spo2,heartrate)
+    VALUES(${device_id},${pname},${pmobile},${spo2},${heartrate})
+  `;
+
+  res.json({ success:true });
+}
 
