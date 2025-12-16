68586db49a5378674c50a973fa6810e218e4a70d
anwar79aldleme-maker <anwar79aldleme@gmail.com>
2025-12-16 09:19:22 +0000

    Update update.js

--- api/update.js
+++ api/update.js
@@ -4,7 +4,7 @@ const sql = neon(process.env.DATABASE_URL);
 export default async function handler(req,res){
   const { id, spo2, heartrate } = req.body;
   await sql`
-    UPDATE sensor_data
+    UPDATE informations
     SET spo2=${spo2}, heartrate=${heartrate}
     WHERE id=${id}
   `;
